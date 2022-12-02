2597e47a76bd9e01a009e678d1321122e6f906e8
Mattéo Delabre <git.matteo@delab.re>
2022-12-01 19:59:48 -0500

    output: Show star completion time

--- aoc/output.py
+++ aoc/output.py
@@ -4,6 +4,7 @@ import shutil
 import os
 from datetime import datetime
 from babel.dates import format_datetime, get_timezone, UTC
+from .config import config
 from .data import get_registration, get_leaderboard
 from .rank import rank_entries
 
@@ -12,42 +13,102 @@ DATETIME_FORMAT = "d MMMM yyyy 'à' HH 'h' mm"
 TIMEZONE = get_timezone("America/Montreal")
 
 
+def format_part(value, name):
+    if value == 0:
+        return []
+    else:
+        return [f"{value} {name}"]
+
+
+def format_timedelta(delta):
+    parts = []
+    hours = delta.seconds // (60 * 60)
+    minutes = (delta.seconds % (60 * 60)) // 60
+    seconds = delta.seconds % 60
+
+    parts.extend(format_part(delta.days, "j"))
+    parts.extend(format_part(hours, "h"))
+    parts.extend(format_part(minutes, "min"))
+    parts.extend(format_part(seconds, "s"))
+
+    return " ".join(parts)
+
+
+def get_puzzle_start(day):
+    return datetime(
+        year=config.leaderboard.year, month=12, day=day,
+        hour=5, minute=0, second=0,
+        tzinfo=UTC,
+    )
+
+
+def get_live_puzzles():
+    now = datetime.now(tz=UTC)
+    return [day for day in range(1, 26) if now >= get_puzzle_start(day)]
+
+
+def generate_entry(member):
+    stars = []
+
+    for day in range(1, 26):
+        level = 0
+        status = []
+
+        start_time = get_puzzle_start(day)
+        get_delta = lambda timestamp: format_timedelta(
+            datetime.fromtimestamp(timestamp, tz=UTC) - start_time)
+
+        if str(day) in member["completion_day_level"]:
+            info = member["completion_day_level"][str(day)]
+
+            if "1" in info:
+                level = 1
+                status.append(
+                    "Première partie résolue en "
+                    + get_delta(info["1"]["get_star_ts"])
+                )
+
+            if "2" in info:
+                level = 2
+                status.append(
+                    "Seconde partie résolue en "
+                    + get_delta(info["2"]["get_star_ts"])
+                )
+
+        stars.append({
+            "level": level,
+            "status": "\n".join(status),
+        })
+
+    return {
+        "name": member["name"],
+        "stars": stars,
+        "score": int(member["local_score"]),
+    }
+
+
 def generate_leaderboard(users, leaderboard):
     environment = Environment(loader=FileSystemLoader("./aoc/templates"))
     template = environment.get_template("index.html")
+
+    now = datetime.now(tz=UTC)
+    live_days = get_live_puzzles()
 
     data = defaultdict(list)
 
     for key, member in leaderboard["members"].items():
         if member["name"] in users:
             category = users[member["name"]]
-            stars = [0] * 25
-
-            for index in range(26):
-                if str(index + 1) in member["completion_day_level"]:
-                    status = member["completion_day_level"][str(index + 1)]
-
-                    if "2" in status:
-                        stars[index] = 2
-                    elif "1" in status:
-                        stars[index] = 1
-
-            data[category].append({
-                "name": member["name"],
-                "stars": stars,
-                "score": int(member["local_score"]),
-            })
+            data[category].append(generate_entry(member))
 
     for category, entries in data.items():
         rank_entries(entries)
-
-    live_days = list(range(1, datetime.now(tz=TIMEZONE).day + 1))
 
     return template.render({
         "leaderboards": sorted(data.items()),
         "live_days": live_days,
         "now": format_datetime(
-            datetime.now(tz=UTC),
+            now,
             DATETIME_FORMAT,
             tzinfo=TIMEZONE,
             locale='fr_CA',
